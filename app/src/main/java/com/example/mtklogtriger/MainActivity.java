ee88b527ede48389682e6c8a406a1decbb51ff68
skyu <skyu@ipageon.com>
2020-06-10 13:47:02 +0900

    Initial commits

---
+++ app/src/main/java/com/example/mtklogtriger/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.mtklogtriger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener(
+
+        ) {
+            @Override
+            public void onClick(View v) {
+                ComponentName compName = new ComponentName("com.mediatek.mtklogger","com.mediatek.mtklogger.MainActivity");
+                Intent intent23 = new Intent(Intent.ACTION_MAIN);
+                intent23.addCategory(Intent.CATEGORY_LAUNCHER);
+                intent23.setComponent(compName);
+                startActivity(intent23);
+            }
+        });
+    }
+}
