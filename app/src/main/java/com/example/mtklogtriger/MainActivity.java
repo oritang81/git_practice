99b53ad9169452be35a30845eacc790a65f4a0ac
skyu <skyu@ipageon.com>
2022-09-05 14:35:18 +0900

    git1

--- app/src/main/java/com/example/mtklogtriger/MainActivity.java
+++ app/src/main/java/com/example/mtklogtriger/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i("TAG", "Git 1");
         setContentView(R.layout.activity_main);
 
         btn = findViewById(R.id.btn);
