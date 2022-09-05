483fadcea8c7887f0231afdc232cf826adb87fb2
skyu <skyu@ipageon.com>
2022-09-05 15:05:37 +0900

    git3

--- app/src/main/java/com/example/mtklogtriger/MainActivity.java
+++ app/src/main/java/com/example/mtklogtriger/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         Log.i("TAG", "Git 1");
         Log.i("TAG", "Git 2");
+        Log.i("TAG", "Git 3");
         setContentView(R.layout.activity_main);
 
         btn = findViewById(R.id.btn);
